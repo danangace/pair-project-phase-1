aa9931c5219896d3c7859658adfbacc8da5d91d9
Danang Putra Bahari <danangbahari89@gmail.com>
2019-10-23 10:47:54 +0700

    add controller & partials views

---
+++ controllers/EventController.js
@@ -0,0 +1,5 @@
+class EventController {
+
+}
+
+module.exports = EventController
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,8 @@ routes.get('/', (req, res) => {
 	Event.findAll()
 	.then((data) => {
 		data = data.map(item => item.dataValues);
-		console.log({data});
 		res.render('home', {data});
 	})
 })
-
-// routes.get('/customer/:id', (req, res) => {
-// 	res.render('customer') //show & edit customer's data
-// })
 
 module.exports = routes;
