000a027dfb880612073ade3298fc88f97bb66184
Danang Putra Bahari <danangbahari89@gmail.com>
2019-10-23 22:42:11 +0700

    edit profile ejs

--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -3,8 +3,8 @@ const Customer = require('../models').Customer;
 const Event = require('../models').Event;
 const CustomerEvent = require('../models').CustomerEvent;
 
+// signup
 routes.post('/signUp', (req, res) => {
-	console.log('routes.post(/signUp: ', req.body) //{ email: [ 'Alif', 'alif@gmail.com', 'alif', '0234234' ], password: 'Male', submit: 'Submit' }
 	Customer.create({
 		name: req.body.name,
 		email: req.body.email,
@@ -14,7 +14,6 @@ routes.post('/signUp', (req, res) => {
 		login_status: 0})
 	.then((data) => {
 		let customer = data.dataValues;
-		console.log({customer});
 		res.redirect(`${customer.id}/edit`);
 	})
 	.catch((err) => {
@@ -22,46 +21,34 @@ routes.post('/signUp', (req, res) => {
 	})
 })
 
-routes.post('/logIn', (req, res) => {
-	console.log('routes.post(/logIn: ', req.body) //{ email: [ 'Alif', 'alif@gmail.com', 'alif', '0234234' ], password: 'Male', submit: 'Submit' }
-	Customer.findOne({where: {email: req.body.email, password: req.body.password}})
-	.then((data) => {
-		console.log('username & password match');
-		console.log(data.dataValues);
-		return Customer.update({login_status: 1}, {where: {id: data.dataValues.id}})
-	})
-		.then((data) => {
-			console.log('data: ', data);
-			res.redirect('/');
-		})
-		.catch(err => {
-			console.log('error pas update status login');
-			console.log(err);
-			res.send(err);
-		})
-	.catch((err) => {
-		console.log('error post login')
-		res.send(err);
-	})
-})
-
 routes.get('/:id/edit', (req, res) => {
-
-	console.log('routes.get(/:id/edit');
 	Customer.findByPk(req.params.id)
 	.then((data) => {
 		let customer = data.dataValues;
-		console.log('routes.get(/:id/edit data: ', {customer});
 		res.render('profile', {customer});
 	})
 })
 
+routes.post('/logIn', (req, res) => {
+	Customer.findOne({where: {email: req.body.email, password: req.body.password}})
+	.then((data) => {
+		return Customer.update({login_status: 1}, {where: {id: data.dataValues.id}})
+	})
+		.then((data) => {
+			res.redirect('/');
+		})
+		.catch(err => {
+			res.send(err);
+		})
+	.catch((err) => {
+		res.send(err);
+	})
+})
+
 routes.post('/:id/edit', (req, res) => {
-	console.log('routes.post/:id/edit', req.body)
 	Customer.update(req.body, {where: {id: req.params.id}})
 	.then(data => {
-		console.log('routes.post/:id/edit (data)', data);
-		res.redirect(`/customers/${req.params.id}/edit`);
+		res.redirect(`localhost:3000/`);
 	})
 })
 
